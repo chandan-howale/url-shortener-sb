4d801f73787f81dd7feced83aa0ad5234f7ddda7
chandan-howale-admin <howalechandan@gmail.com>
2026-08-30 17:36:03 +0530

    feat: add URL redirect functionality - Add RedirectController with GET /{shortUrl} endpoint - Add getOriginalUrl() in UrlMappingService - Increment click count on each redirect - Record ClickEvent with timestamp on every redirect - Return 302 redirect to original URL

--- src/main/java/com/url/shortener/service/UrlMappingService.java
+++ src/main/java/com/url/shortener/service/UrlMappingService.java
@@ -92,4 +92,21 @@ public class UrlMappingService {
                         Collectors.counting()));
 
     }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+
+        if (urlMapping != null) {
+            urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+            urlMappingRepository.save(urlMapping);
+
+            //record click event
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
+
+        return urlMapping;
+    }
 }
